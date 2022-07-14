40d0742943d61d6e96431401661f777e6c927055
venkatr999 <86608276+venkatr999@users.noreply.github.com>
2022-07-14 17:18:51 +0530

    Time: 220 ms (5.10%), Space: 13.8 MB (60.10%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        for i in range(1000):
+            s=0
+            t=n
+            while(t>0):
+                r=t%10
+                s+=r*r
+                t=t//10
+            if(s==1):
+                return True
+            n=s 
+        return False
+        
