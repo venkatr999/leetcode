9794ed5d5782d4b278f99ba0c92fb7d0c0108d82
venkatr999 <86608276+venkatr999@users.noreply.github.com>
2022-07-20 21:46:59 +0530

    Time: 3 ms (62.03%), Space: 5.8 MB (73.85%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -3,7 +3,7 @@ public:
     int addDigits(int num) {
         if(num==0)
             return 0;
-        if(num%9==0)
+        else if(num%9==0)
             return 9;
         else
             return num%9;
