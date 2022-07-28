2c276da68d73741af661d634f78e586eb5af544c
venkatr999 <86608276+venkatr999@users.noreply.github.com>
2022-07-28 23:18:39 +0530

    Time: 146 ms (11.00%), Space: 15.3 MB (45.56%) - LeetHub

---
+++ 942-di-string-match/942-di-string-match.py
@@ -0,0 +1,12 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        l,r = 0,len(s)
+        perm = []
+        for char in s:
+            if char =='I':
+                perm.append(l)
+                l+=1
+            else:
+                perm.append(r)
+                r-=1
+        return perm + [r]
