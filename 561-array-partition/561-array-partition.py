d1355685451e64fe23f38dcf1c977d7f622f81a9
venkatr999 <86608276+venkatr999@users.noreply.github.com>
2022-07-28 23:51:55 +0530

    Time: 328 ms (74.78%), Space: 16.7 MB (78.40%) - LeetHub

--- 561-array-partition/561-array-partition.py
+++ 561-array-partition/561-array-partition.py
@@ -2,8 +2,7 @@ class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
         res=0
-        n=len(nums)
-        for i in range(0,n,2):
+        for i in range(0,len(nums),2):
             res+=min(nums[i],nums[i+1])
         return res
             
